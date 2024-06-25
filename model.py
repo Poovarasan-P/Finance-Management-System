415d03a8a32aa136d60b9a01ec2e241d5892c438
Poovarasan-P <147720783+Poovarasan-P@users.noreply.github.com>
2024-06-25 22:04:08 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,24 @@
+# backend/app/models.py
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(50), nullable=False)  # 'credit' or 'debit'
+    category = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    transaction_date = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
